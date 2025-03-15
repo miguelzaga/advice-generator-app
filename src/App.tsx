813beabbed6e9c8a164b8056d71b84577f391bef
Miguel Zapata <miguezaga2@hotmail.com>
2025-03-16 00:24:43 +1100

    added fonts and border-radius

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import patternDesktop from "./images/pattern-divider-desktop.svg";
 function App() {
   return (
     <>
-      <div className="absolute inset-0 bg-blue-900 px-4">
-        <div className="mx-auto my-[120px] max-w-[540px] bg-blue-700 px-6 pt-10 pb-16 text-center lg:my-[223px]">
-          <h1 className="text-green-300">ADVICE #117</h1>
-          <p className="text-blue-200">
+      <div className="font-main absolute inset-0 bg-blue-900 px-4">
+        <div className="relative mx-auto my-[120px] max-w-[540px] rounded-md rounded-sm bg-blue-700 px-6 pt-10 pb-16 text-center lg:my-[223px]">
+          <h1 className="text-heading-sm md:text-heading-md font-black text-green-300">
+            ADVICE #117
+          </h1>
+          <p className="text-quote-sm md:text-quote-md font-black text-blue-200">
             “It is easy to sit up and take notice, what's difficult is getting
             up and taking action.”
           </p>
-          <img className="mx-auto" src={pattern} alt="" />
-          <img className="hidden" src={patternDesktop} alt="" />
-          <button className="size-16 bg-green-300">
+          <div className="mx-auto max-w-fit">
+            <img className="md:hidden" src={pattern} alt="" />
+            <img className="max-md:hidden" src={patternDesktop} alt="" />
+          </div>
+          <button className="absolute -bottom-8 left-[calc(50%-2rem)] flex size-16 items-center justify-center rounded-full bg-green-300">
             <img src={dice} alt="icon of a dice" />
           </button>
         </div>
