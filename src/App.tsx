9f4cb9b8b66ee8745efb57565b4d50262382d06b
Miguel Zapata <miguezaga2@hotmail.com>
2025-03-16 00:33:13 +1100

    updated spacing

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ function App() {
   return (
     <>
       <div className="font-main absolute inset-0 bg-blue-900 px-4">
-        <div className="relative mx-auto my-[120px] max-w-[540px] rounded-md rounded-sm bg-blue-700 px-6 pt-10 pb-16 text-center lg:my-[223px]">
+        <div className="relative mx-auto my-[120px] max-w-[540px] rounded-md rounded-sm bg-blue-700 px-6 pt-10 pb-16 text-center md:px-12 md:pt-12 md:pb-18 lg:my-[223px]">
           <h1 className="text-heading-sm md:text-heading-md font-black text-green-300">
             ADVICE #117
           </h1>
-          <p className="text-quote-sm md:text-quote-md font-black text-blue-200">
+          <p className="text-quote-sm md:text-quote-md mt-6 font-black text-blue-200">
             “It is easy to sit up and take notice, what's difficult is getting
             up and taking action.”
           </p>
-          <div className="mx-auto max-w-fit">
+          <div className="mx-auto mt-6 max-w-fit md:mt-10">
             <img className="md:hidden" src={pattern} alt="" />
             <img className="max-md:hidden" src={patternDesktop} alt="" />
           </div>
